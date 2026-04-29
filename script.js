5f1294789034e89efada53b04951d1e3f77d9d54
ahkamal321-ak <ahkamal321@gmail.com>
2026-04-29 17:54:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function registerUser(event) {
+    event.preventDefault();
+
+    let fullName = document.getElementById("fullName").value;
+    let email = document.getElementById("signupEmail").value;
+    let password = document.getElementById("signupPassword").value;
+    let role = document.getElementById("role").value;
+
+    localStorage.setItem("fullName", fullName);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    localStorage.setItem("role", role);
+
+    alert("Account Created Successfully!");
+    window.location.href = "index.html";
+}
+
+function loginUser(event) {
+    event.preventDefault();
+
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
+
+    let savedEmail = localStorage.getItem("email");
+    let savedPassword = localStorage.getItem("password");
+
+    if (email === savedEmail && password === savedPassword) {
+        alert("Login Successful!");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Wrong Email or Password!");
+    }
+}
