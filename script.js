bc3500e23bcadb1a85eb5bee87b4d5ab59791315
ahkamal321-ak <ahkamal321@gmail.com>
2026-04-30 13:57:19 +0300

    Refactor user registration and login functions

--- script.js
+++ script.js
@@ -31,3 +31,22 @@ function loginUser(event) {
         alert("Wrong Email or Password!");
     }
 }
+
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+function loadTheme() {
+    let theme = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+}
